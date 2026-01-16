924c6980195e4e20a4f16700fc2482b957c3a362
Munna <munna@Munnas-MacBook-Air.local>
2026-01-16 19:57:09 +0600

    sum and even number finedout

--- Loop.js
+++ Loop.js
@@ -21,9 +21,41 @@
 
 
 
-let num = 1;
-while (num <= 10){
+// let num = 1;
+// while (num <= 10){.   focus on the condition 
 
-    console.log(num);
-    num++;
-}
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 50;
+// while (num <= 100){
+//  console.log(num);
+//     num++;
+// }
+
+
+
+// let num = 1;
+// let sum = 0;
+// while( num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum:', sum);
+//     num++;
+// }
+
+
+// even Number
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num)
+    
+//     if(num % 2 === 0){
+//         console.log('even num', num)
+
+//     }
+//     num++
+// }
